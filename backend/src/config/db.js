d7f942ffa4ea9f1cff0a88e8e594a5d24ef77872
Minh-begintolovecoding <ducminh2003tp@gmail.com>
2025-08-01 00:38:32 +0700

    Create .env file: store mongoDB api and install dotenv, mongoose

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 export const connectDB = async() =>{
     try {
-        await mongoose.connect("mongodb+srv://ducminh2003tp:nNFJ2kRtK9i6wCBm@cluster0.ezbortw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        console.log("MONGO_URI:", process.env.MONGO_URI); // ← kiểm tra giá trị
+
+        await mongoose.connect(process.env.MONGO_URI);
         console.log(" MONGODB CONNECTED SUCCESSFULLY!");
     }catch (error) {
         console.error("Error connecting to MONGODB",error);
         process.exit(1)
     }
-}
+};
